ce8701b673be5debae3e6c8423f62e6078480ef8
Moshood Salawudeen <86296143+Moshood-Wale@users.noreply.github.com>
2022-02-19 14:23:59 +0100

    Update process_csv_file.py

--- process_csv_file.py
+++ process_csv_file.py
@@ -12,17 +12,8 @@ def file_opening(filename):
             return None
     except FileNotFoundError:
         return None
+    
 path = "posts.csv"
 header_row = file_opening(path)[0]
-header_array = header_row[1:].lstrip().split()
 body_rows = file_opening(path)[1:]
-
-# def extract_header():
-
-#     try:
-#         listed_lines = file_opening()
-#         header = f"{listed_lines[0]}"
-#         create_schema = 
-#     except:
-#         return("This file is not supported or found")
-
+print(header_row)
